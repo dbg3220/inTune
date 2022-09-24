54e701f00e59d66a075b7cf23f59799c2399c170
Jonathan Zhu <jz8089@rit.edu>
2022-09-23 20:13:32 -0400

    MVVM Code for Searching products

--- estore-api/src/main/java/estoreapi/viewmodel/ProductController.java
+++ estore-api/src/main/java/estoreapi/viewmodel/ProductController.java
@@ -22,7 +22,8 @@ import java.util.logging.Logger;
 /**
  * Handles the REST API requests for the product resource
  * <p>
- * {@literal @}RestController Spring annotation identifies this class as a REST API
+ * {@literal @}RestController Spring annotation identifies this class as a REST
+ * API
  * method handler to the Spring framework
  * 
  * @author Hayden Cieniawski
@@ -37,62 +38,64 @@ public class ProductController {
     /**
      * Creates a REST API controller to respond to requests
      * 
-     * @param ProductDao The {@link ProductDAO Product Data Access Object} to perform CRUD operations
-     * <br>
-     * This dependency is injected by the Spring Framework
+     * @param ProductDao The {@link ProductDAO Product Data Access Object} to
+     *                   perform CRUD operations
+     *                   <br>
+     *                   This dependency is injected by the Spring Framework
      */
     public ProductController(ProductDAO productDao) {
         this.productDao = productDao;
     }
 
-   
     // @GetMapping("/{id}")
     // public ResponseEntity<Product> getProduct(@PathVariable int id) {
-    //     LOG.info("GET /Products/" + id);        
+    // LOG.info("GET /Products/" + id);
     // }
 
-   
     // @GetMapping("")
     // public ResponseEntity<Product[]> getProducts() {
-    //     LOG.info("GET /products");
+    // LOG.info("GET /products");
 
-    //     }
-
-
-    // @GetMapping("/")
-    // public ResponseEntity<Product[]> searchProducts(@RequestParam String name) {
-    //     LOG.info("GET /Productes/?name="+name);
-       
     // }
 
-   
-    @PostMapping("")
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        LOG.info("POST /products " + product);
+    @GetMapping("/")
+    public ResponseEntity<Product[]> searchProducts(@RequestParam String name) {
+        LOG.info("GET /Productes/?name=" + name);
+
         try {
-            Product newProduct= productDao.createProduct(product);
-            if (newProduct != null)
-                return new ResponseEntity<Product>(newProduct,HttpStatus.CREATED);
+            Product[] products = productDao.findProducts(name);
+            if (products != null)
+                return new ResponseEntity<Product[]>(products, HttpStatus.OK);
             else
-                return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        catch(IOException e) {
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    
+    @PostMapping("")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        LOG.info("POST /products " + product);
+        try {
+            Product newProduct = productDao.createProduct(product);
+            if (newProduct != null)
+                return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
+            else
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     // @PutMapping("")
     // public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
-    //     LOG.info("PUT /Productes " + product);
+    // LOG.info("PUT /Productes " + product);
     // }
 
-   
     // @DeleteMapping("/{id}")
     // public ResponseEntity<Product> deleteProduct(@PathVariable int id) {
-    //     LOG.info("DELETE /Productes/" + id);
+    // LOG.info("DELETE /Productes/" + id);
     // }
 }
-
-     
