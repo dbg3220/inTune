e2a4c092346cb264a6a16f33073db55906bdedf3
dbg3220 <90785065+dbg3220@users.noreply.github.com>
2022-10-04 17:06:32 -0400

    fixed minor doc errors

--- estore-api/src/main/java/estoreapi/viewmodel/UserController.java
+++ estore-api/src/main/java/estoreapi/viewmodel/UserController.java
@@ -30,10 +30,10 @@ import estoreapi.persistence.UserDAO;
  */
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("users")
 public class UserController {
-    
-    private static final Logger LOG = Logger.getLogger(ProductController.class.getName());
+
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
 
     private UserDAO userDAO;
 
@@ -41,60 +41,63 @@ public class UserController {
      * Creates a REST API controller to respond to requests
      * 
      * @param UserDAO The {@link UserDAO User Data Access Object} to
-     *                   perform CRUD operations
-     *                   <br>
-     *                   This dependency is injected by the Spring Framework
+     *                perform CRUD operations
+     *                <br>
+     *                This dependency is injected by the Spring Framework
      */
-    public UserController(UserDAO userDAO){
+    public UserController(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
 
     /**
-    * Handles the HTTP GET request for the user resource
-    * @param id The id of the user to retrieve
-    * @return The user with the specified id
-    */
+     * Handles the HTTP GET request for the user resource
+     * 
+     * @param id The id of the user to retrieve
+     * @return The user with the specified id
+     */
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable int id) {
-        LOG.info("GET /Products/" + id);   
+        LOG.info("GET /users/" + id);
         try {
             User user = userDAO.getUser(id);
             if (user != null)
                 return new ResponseEntity<>(user, HttpStatus.OK);
             else
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
-        } catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     /**
      * Handles the HTTP GET request for the user resource
+     * 
      * @return All users
      */
     @GetMapping("")
-    public ResponseEntity<User[]> getUsers(){
+    public ResponseEntity<User[]> getUsers() {
         try {
             User[] users = userDAO.getUsers();
             return new ResponseEntity<>(users, HttpStatus.OK);
-        } catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     /**
      * Handles the HTTP GET request for searching for a user
+     * 
      * @param username The string to match against
      * @return The user if found, null otherwise
      */
     @GetMapping("/{username}")
-    public ResponseEntity<User> searchForUser(@RequestParam String username){
+    public ResponseEntity<User> searchForUser(@RequestParam String username) {
         LOG.info("GET /users/?username=" + username);
         try {
             User user = userDAO.findUser(username);
-            if(user != null){
+            if (user != null) {
                 return new ResponseEntity<>(user, HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -108,15 +111,15 @@ public class UserController {
     /**
      * Handles the HTTP POST request to create a new user
      * 
-     * @param product The user to create
+     * @param user The user to create
      * @return The HTTP response
      */
     @PostMapping("")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         LOG.info("POST /users " + user);
         try {
-            if(user.getccnum().length() != 16
-                || user.isAdmin()){//checks valid credit card number length and if the user is admin
+            if (user.getccnum().length() != 16
+                    || user.isAdmin()) {// checks valid credit card number length and if the user is admin
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             User newUser = userDAO.createUser(user); // use conditionals to check the is
@@ -132,6 +135,7 @@ public class UserController {
 
     /**
      * Handles the HTTP PUT request to update an existing user
+     * 
      * @param user The user to update
      * @return The HTTP response
      */
@@ -139,8 +143,8 @@ public class UserController {
     public ResponseEntity<User> updateUser(@RequestBody User user) {
         LOG.info("POST /users " + user);
         try {
-            if(user.getccnum().length() != 16
-                || user.isAdmin()){//checks valid credit card number length and if the user is admin
+            if (user.getccnum().length() != 16
+                    || user.isAdmin()) {// checks valid credit card number length and if the user is admin
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             User newUser = userDAO.updateUser(user); // use conditionals to check the is
@@ -156,6 +160,7 @@ public class UserController {
 
     /**
      * Handles the HTTP DELETE request
+     * 
      * @param id The id of the user to delete
      * @return The HTTP response
      */
@@ -164,7 +169,7 @@ public class UserController {
         LOG.info("DELETE /users/ " + id);
         try {
             boolean result = userDAO.deleteUser(id);
-            if(result){
+            if (result) {
                 return new ResponseEntity<>(HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
@@ -174,5 +179,5 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
 }
