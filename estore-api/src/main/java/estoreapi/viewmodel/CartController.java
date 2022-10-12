ae1ed5bd8794711c6b664c2e9efb91c12a444dad
ClaytonAcheson <cra7469@rit.edu>
2022-10-12 12:58:18 -0400

    Update CartController.java
    Started implementing the CartController

--- estore-api/src/main/java/estoreapi/viewmodel/CartController.java
+++ estore-api/src/main/java/estoreapi/viewmodel/CartController.java
@@ -1,5 +1,36 @@
 package estoreapi.viewmodel;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import estoreapi.model.Cart;
+import estoreapi.persistence.CartDAO;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Handles the REST API requests for the Cart resource
+ * <p>
+ * {@literal @}RestController Spring annotation identifies this class as a REST
+ * API
+ * method handler to the Spring framework
+ * 
+ * @author Clayton Acheson
+ */
+
+@RestController
+@RequestMapping("Carts")
 public class CartController {
-    
-}
+    private static final Logger LOG = Logger.getLogger(CartController.class.getName());
+    private CartDAO CartDao;
