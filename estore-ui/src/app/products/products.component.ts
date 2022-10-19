05528c68cf908dc1bae268f61d78e1036e90ac25
JZ8089 <jz8089@rit.edu>
2022-10-18 23:29:59 -0400

    Created Search Bar and Filter Options with Product Service
    Created Search Bar and Filter Options with Product Service
    Added functionality in product service for common shared data and resetting filters

--- estore-ui/src/app/products/products.component.ts
+++ estore-ui/src/app/products/products.component.ts
@@ -14,22 +14,26 @@ export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
 
-  selectedProduct?: Product;
 
+  selectedProduct?: Product;
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
     this.messageService.add(`ProductsComponent: Selected product id=${product.id}`);
   }
-  constructor(private productService: ProductService, 
+
+  constructor(private productService: ProductService,
     private messageService: MessageService) { }
 
-  getProducts(): void {
-    this.productService.getProducts().subscribe(products => this.products = products);
-  }
-
   ngOnInit(): void {
-    this.getProducts();
+    this.productService.getProducts().subscribe((products => {
+      this.products = products;
+    } ));
+    // setTimeout(() => {
+    //     this.products = [];
+    //     console.log("Delayed for 1 second.");
+    //   }, 3000
+    // )
   }
 
 }
