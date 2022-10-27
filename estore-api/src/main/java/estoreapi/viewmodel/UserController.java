9d4257af9346f025d4cf62b5ffe81ee710d605bc
Damon Gonzalez <dbg3220@rit.edu>
2022-10-26 21:08:01 -0400

    Updated HttpStatus codes

--- estore-api/src/main/java/estoreapi/viewmodel/UserController.java
+++ estore-api/src/main/java/estoreapi/viewmodel/UserController.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,7 +64,7 @@ public class UserController {
             if (user != null)
                 return new ResponseEntity<>(user, HttpStatus.OK);
             else
-                return new ResponseEntity<>(HttpStatus.CONFLICT);
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (IOException e) {
             LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -136,6 +134,7 @@ public class UserController {
             }
             Cart newCart = cartDAO.createCart(newUser.getId());
             if(newCart == null){
+                userDAO.deleteUser(newUser.getId());//delete the newly created user to compensate
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
             }
             return new ResponseEntity<User>(newUser, HttpStatus.OK);
