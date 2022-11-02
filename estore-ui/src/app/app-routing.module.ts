a966252a3d971c89172d990242f8494444326708
JZ8089 <jz8089@rit.edu>
2022-11-01 20:07:37 -0400

    Hooked up checkout button to component
    Added to routes and hooked up button click to component route

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
+import {CheckoutComponent} from "./checkout/checkout.component";
 
 const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  {path: 'detail/:id', component: ProductDetailComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'cart', component: CartComponent}
-
+  { path: 'detail/:id', component: ProductDetailComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'cart', component: CartComponent},
+  { path: 'checkout', component: CheckoutComponent }
 ];
 
 @NgModule({
--- estore-ui/src/app/cart/cart.component.ts
+++ estore-ui/src/app/cart/cart.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { MessengerService } from '../messenger.service';
-import { Product } from '../product';
+import {Component, OnInit} from '@angular/core';
+import {MessengerService} from '../messenger.service';
+import {Product} from '../product';
 import {filter, Subject, takeUntil} from "rxjs";
 import {ProductService} from "../product.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cart',
@@ -12,15 +13,15 @@ import {ProductService} from "../product.service";
 })
 export class CartComponent implements OnInit {
 
-  cartItems : Product[] = [];
+  cartItems: Product[] = [];
   componentDestroyed$ = new Subject();
   subTotals: number = 0;
-  subQuantity: number =0;
+  subQuantity: number = 0;
 
-  constructor(private msg: MessengerService,private productService: ProductService) { }
+  constructor(private msg: MessengerService, private productService: ProductService, private router: Router) {
+  }
 
-  removeProductCart(product: Product)
-  {
+  removeProductCart(product: Product) {
     this.productService.removeToCart(product);
     // when removed, set subTotals and subQuantity to 0 to refresh the amount
     // Call subTotal() to get the new subTotals and subQuantityS
@@ -29,27 +30,28 @@ export class CartComponent implements OnInit {
     this.subTotal();
 
   }
-  subTotal()
-  {
-    for(let product of this.cartItems)
-    {
+
+  subTotal() {
+    for (let product of this.cartItems) {
       this.subTotals = this.subTotals + (product.price * product.quantity);
       this.subTotals = Number(parseFloat(String(this.subTotals)).toFixed(2));
       this.subQuantity = this.subQuantity + product.quantity;
     }
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.productService.getCart().pipe(filter(cart => !!cart), takeUntil(this.componentDestroyed$))
-      .subscribe(cartItems =>{
+      .subscribe(cartItems => {
         this.cartItems = cartItems
         this.subTotal();
       });
 
-    this.msg.getMsg().subscribe( product => {
+    this.msg.getMsg().subscribe(product => {
       console.log(product)
+    })
+  }
 
-  })
-
-}
+  routeToCheckoutComponent() {
+    this.router.navigate(['/checkout']);
+  }
 }
