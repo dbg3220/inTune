9f5e88c5f840d5a924637e060ca3faac58cbeded
Damon Gonzalez <dbg3220@rit.edu>
2022-11-07 10:15:16 -0500

    Added delete to lessons component

--- estore-ui/src/app/lesson.service.ts
+++ estore-ui/src/app/lesson.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Lesson } from './lesson';
@@ -13,14 +13,23 @@ export class LessonService {
 
   private lessonsURL = 'http://localhost:8080/lessons';  // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   getLessons(): Observable<Lesson[]> {
     this.messageService.add('LessonService: fetched lessons')
-    return this.http.get<Lesson[]>(this.lessonsURL);
+    return this.http.get<Lesson[]>(this.lessonsURL, this.httpOptions);
   }
 
   getLesson(id: Number): Observable<Lesson>{
     this.messageService.add('LessonService: fetched lesson with id=' + id);
     const url = `${this.lessonsURL}/${id}`;
-    return this.http.get<Lesson>(url);
+    return this.http.get<Lesson>(url, this.httpOptions);
+  }
+
+  deleteLesson(id: Number) {
+    const url = `${this.lessonsURL}/${id}`;
+    return this.http.delete<Lesson>(url, this.httpOptions);
   }
 }
--- estore-ui/src/app/lessons/lessons.component.ts
+++ estore-ui/src/app/lessons/lessons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Lesson } from '../lesson';
 import { LessonService } from '../lesson.service';
 import { MessageService } from '../message.service';
@@ -12,23 +13,50 @@ export class LessonsComponent implements OnInit {
 
   lessons: Lesson[] = [];
   selectedLesson?: Lesson;
+  promptLogin: boolean = false;
 
   constructor(private lessonService: LessonService,
-    private messageService: MessageService) { }
+    private messageService: MessageService, private aRoute: ActivatedRoute, 
+    private router: Router) {
+      router.events.subscribe(
+        (event) => {
+          if(event instanceof NavigationEnd){
+            this.promptLogin = false;
+          }
+        }
+      )
+     }
 
   ngOnInit(): void {
+    this.messageService.add('LessonComponent has been initialized');
     this.getLessons();
   }
 
   getLessons() {
+    this.messageService.add('LessonComponent: subscribed to lesson service')
     this.lessonService.getLessons().subscribe(lessons => this.lessons = lessons);
+  }
+
+  availableLessons() {
+    return this.lessons.filter(lesson => lesson.isFull == false);
   }
 
   onSelect(lesson: Lesson) {
     this.messageService.add(`LessonsComponent: Selected lesson id=${lesson.id}`)
     console.log("Selected lesson has id=" + lesson.id);
     this.selectedLesson = lesson;
-    //if(user is not logged in or is admin this action cannot be allowed)
-    //TODO
+    const currentUserID = 1;//TODO replace with function that keeps track of current user
+    const isAdmin = true;//TODO replace with function that keeps rack of admin status
+    if(false && !isAdmin){//Adjust if statement with new functions
+
+    } else {
+      this.promptLogin = true;
+    }
+  }
+
+  delete(lesson: Lesson){
+    this.messageService.add(`LessonComponent: Deleted lesson id=${lesson.id}`)
+    this.lessons = this.lessons.filter(l => l!== lesson);
+    this.lessonService.deleteLesson(lesson.id).subscribe();
   }
 }
