450f1edbff17911ad596964d683f7c7c804e832b
JZ8089 <89427876+JZ8089@users.noreply.github.com>
2022-11-13 21:35:45 -0500

    Code Coverage
    Fixed cart to 91%

--- estore-api/src/test/java/com/estore/api/estoreapi/model/CartTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/CartTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import estoreapi.model.Cart;
+import org.yaml.snakeyaml.events.Event;
 
 /**
  * The unit test suite for the Cart class
@@ -58,13 +59,76 @@ public class CartTest {
     }
 
     @Test
-    public void testQuantityId()
+    public void testsetId()
     {
         // Setup
-        int testing_ID = 12;
-        //Invoke
-        Cart testCart = new Cart(testing_ID);
-        assertEquals(testing_ID,testCart.getQuantity(testing_ID));
+        int expectedId = 3;
+        // Invoke
+        Cart testCart = new Cart(20);
+        testCart.setId(3);
+
+        // Analyze
+        assertEquals(expectedId, testCart.getId());
     }
+
+    @Test
+    public void testContainsId()
+    {
+        // Setup
+        int Id = 2;
+        // Invoke
+        Cart testCart = new Cart(2);
+
+        // Analyze
+        assertEquals(false, testCart.containsProduct(Id));
+    }
+
+    @Test
+    public void testGetQuantity()
+    {
+        // Setup
+        int quantity = -1;
+        int id = 2;
+        ArrayList<Integer> product= new ArrayList<Integer>();
+        // Invoke
+        ArrayList<Integer> quantities= new ArrayList<Integer>();
+        quantities.add(3);
+        Cart testCart = new Cart(id,product,quantities);
+
+        // Analyze
+        assertEquals(quantity, testCart.getQuantity(id));
+    }
+
+
+    @Test
+    public void testEquals()
+    {
+        // Setup
+        int idF = 3;
+
+        Cart testCart = new Cart(idF);
+        Cart testCart2 = new Cart(idF);
+
+
+        // Analyze
+        assertEquals(true, testCart.equals(testCart2));
+    }
+    @Test
+    public void testEqualsFalse()
+    {
+        // Setup
+        int idF = 3;
+        int idS = 4;
+
+        Cart testCart = new Cart(idF);
+        Cart testCart2 = new Cart(idS);
+
+
+        // Analyze
+        assertEquals(false, testCart.equals(testCart2));
+    }
+
+
+
 
 }
