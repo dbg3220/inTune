25721bec341f334fc3ce62825b534148755c741f
Damon Gonzalez <dbg3220@rit.edu>
2022-11-15 10:54:17 -0500

    Added sorting function that sorts based on only weekday of a Lesson

--- estore-ui/src/app/lessons/lessons.component.ts
+++ estore-ui/src/app/lessons/lessons.component.ts
@@ -81,16 +81,8 @@ export class LessonsComponent implements OnInit {
   /** Sorts and returns a given array of lessons by day of the week and time of day(MOND->FRI,9->5)  */
   sort(lessons: Lesson[]): Lesson[] {
      var result: Lesson[] = lessons.sort((lesson1, lesson2) => {
-      var day1 = this.dayToValue(lesson1.weekday);//The integer value of lesson1's weekday
-      var day2 = this.dayToValue(lesson2.weekday);//The integer value of lesson2's weekday
-      if(day1 > day2){
-        return 1;
-      } else if(day1 < day1){
-        return -1;
-      } 
-      return 0;
+      return this.compareDay(lesson1.weekday, lesson2.weekday);
     });
-    console.log(result);
     return result;
   }
 
@@ -120,14 +112,14 @@ export class LessonsComponent implements OnInit {
 
   /** Adds a lesson to the inventory */
   addLesson(category: String, instructor: String, weekday: String,
-            startTime: Number, price: Number, name: String) {
+            startTime: number, price: number, name: String) {
     name = name.trim();
     this.addLessonHelp(-1, false, category, instructor, weekday, startTime, -1, price, name);
   }
 
   /** Helper method to lesson that calls the lesson service */
-  private addLessonHelp(id: Number, isFull: boolean, category: String, 
-                instructor: String, weekday: String, startTime: Number, 
+  private addLessonHelp(id: number, isFull: boolean, category: String, 
+                instructor: String, weekday: String, startTime: number, 
                 userID: Number, price: Number, name: String) {
     var newLesson = { id, isFull, category, instructor, weekday, startTime, userID, price, name } as Lesson;
     this.lessonService.addLesson(newLesson).subscribe(() => {
@@ -154,13 +146,16 @@ export class LessonsComponent implements OnInit {
     return lesson.isFull;
   }
 
-  /** Assigns a weekday value to an integer so it can be used to sort lessons, if no day found returns -1 */
-  private dayToValue(day: String): Number {
-    for(let i = 0; i < this.potentialDays.length; i++) {
-      if(day == this.potentialDays[i]){
-        return i + 1;
-      }
+  /** Compares a day of the week to the other, returning -1, 0, 1 based on how a lesson should be sorted */
+  private compareDay(day1: String, day2: String): number{
+    var index1 = this.potentialDays.indexOf(day1);
+    var index2 = this.potentialDays.indexOf(day2);
+    if(index1 == index2){
+      return 0;
+    } else if(index1 > index2){
+      return 1;
+    } else {
+      return -1;
     }
-    return -1;
   }
 }
