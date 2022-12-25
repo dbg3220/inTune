affe3623d7f131e2386252d92525c0cabdf291f3
Damon Gonzalez <dbg3220@rit.edu>
2022-12-25 13:49:49 -0500

    Updated UserFileDAO to mimic other daos

--- estore-api/src/main/java/estoreapi/persistence/UserFileDAO.java
+++ estore-api/src/main/java/estoreapi/persistence/UserFileDAO.java
@@ -2,51 +2,50 @@ package estoreapi.persistence;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import estoreapi.model.User;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import estoreapi.model.Cart;
-import estoreapi.model.User;
-
 /**
- * Implements methods described in UserDAO
- * 
- * @author Damon Gonzalez
- */
+* Implements methods described in UserDAO
+* 
+* @author Damon Gonzalez
+*/
 @Component
-public class UserFileDAO implements UserDAO{
-    Map<Integer,User> users;   // Provides a local cache of the user objects
-                                           // so that we don't need to read from the file
-                                           // each time
-    Map<Integer,Cart> carts;   // Provides a local cache of the Cart objects
-                                           // so that we don't need to read from the file
-                                           // each time
-    private ObjectMapper objectMapper;  // Provides conversion between user
-                                        // objects and JSON text format written
-                                        // to the file
-    private static int nextId;  // The next Id to assign to a new user
-    private String filename;    // Filename to read from and write to
+public class UserFileDAO implements UserDAO {
+
+    /** The map storing users that are mapped from their id */
+    Map<Integer, User> users;
+    /** Class used to write java objects to json files */
+    private ObjectMapper objectMapper;
+    /** The next id of a user, to be only accessed through nextId() */
+    private static int nextId;
+    /** The file to read/write to */
+    private String filename;
 
     /**
-     * Creates a user File Data Access Object
-     * 
-     * @param filename Filename to read from and write to
-     * @param objectMapper Provides JSON Object to/from Java Object serialization and deserialization
-     * 
+     * Public constructor for UserFileDAO
+     * @param filename The path of the file to read/write
+     * @param objectMapper Provides JSON Object serialization and deserialization
      * @throws IOException when file cannot be accessed or read from
      */
-    public UserFileDAO(@Value("${users.file}") String filename,ObjectMapper objectMapper) throws IOException {
+    public UserFileDAO(@Value("${users.file}") String filename,
+                          ObjectMapper objectMapper) throws IOException {
         this.filename = filename;
         this.objectMapper = objectMapper;
-        load();  // load the users from the file
+        load();
     }
 
     /**
-     * Generates the next id for a new {@linkplain User user}
+     * Provides the next id for a user to be created with in order
+     * to preserve uniqueness.
      * 
      * @return The next id
      */
@@ -57,135 +56,95 @@ public class UserFileDAO implements UserDAO{
     }
 
     /**
-     * Generates an array of {@linkplain User users} from the tree map
-     * 
-     * @return  The array of {@link User users}, may be empty
-     */
-    private User[] getUsersArray() {
-        return getUsersArray(null);
-    }
-
-    /**
-     * Generates an array of {@linkplain User users} from the tree map for any
-     * {@linkplain User users} whose username contains the text specified by containsText
-     * <br>
-     * If containsText is null, the array contains all of the {@linkplain User users}
-     * in the tree map
-     * 
-     * @return  The array of {@link User users}, may be empty
-     */
-    private User[] getUsersArray(String containsText) { // if containsText == null, no filter
-        ArrayList<User> userArrayList = new ArrayList<>();
-        
-
-        for (User user : users.values()) {
-            if (containsText == null || user.getUsername().contains(containsText)) {
-                userArrayList.add(user);
-            }
-        }
-
-        User[] userArray = new User[userArrayList.size()];
-        userArrayList.toArray(userArray);
-        return userArray;
-    }
-
-    /**
-     * Saves the {@linkplain User users} from the map into the file as an array of JSON objects
-     * 
-     * @return true if the {@link User users} were written successfully
-     * 
+     * Saves user objects in the map of users to file storage
      * @throws IOException when file cannot be accessed or written to
      */
-    private boolean save() throws IOException {
+    private void save() throws IOException {
         User[] userArray = getUsersArray();
-
-        // Serializes the Java Objects to JSON objects into the file
-        // writeValue will thrown an IOException if there is an issue
-        // with the file or reading from the file
         objectMapper.writeValue(new File(filename),userArray);
-        return true;
     }
 
     /**
-     * Loads {@linkplain User users} from the JSON file into the map
-     * <br>
-     * Also sets next id to one more than the greatest id found in the file
-     * 
-     * @return true if the file was read successfully
-     * 
+     * Loads user objects from file storage
      * @throws IOException when file cannot be accessed or read from
      */
-    private boolean load() throws IOException {
+    private void load() throws IOException {
         users = new TreeMap<>();
         nextId = 0;
-        // Deserializes the JSON objects from the file into an array of users
-        // readValue will throw an IOException if there's an issue with the file
-        // or reading from the file
         User[] userArray = objectMapper.readValue(new File(filename),User[].class);
-
-        // Add each user to the tree map and keep track of the greatest id
         for (User user : userArray) {
             users.put(user.getId(), user);
             if (user.getId() > nextId)
                 nextId = user.getId();
         }
-        // Make the next id one greater than the maximum from the file
-        ++nextId;
-        return true;
-        
+        ++nextId;//make the next id greater than the last
+    }
+    
+    /**
+     * Generates an array of users from the map of users
+     * 
+     * @return The array of users, may be empty
+     */
+    private User[] getUsersArray() {
+        ArrayList<User> userArrayList = new ArrayList<>();
+        for (User user : users.values()) {
+            userArrayList.add(user);
+        }
+        User[] userArray = new User[userArrayList.size()];
+        userArrayList.toArray(userArray);
+        return userArray;
     }
 
     @Override
-    public User[] getUsers() throws IOException {
-        synchronized(users){
+    public User[] getUsers() throws IOException{
+        synchronized(users) {
             return getUsersArray();
         }
     }
 
     @Override
-    public User getUser(int id) throws IOException {
-        synchronized(users){
-            if(users.containsKey(id)){
-                return users.get(id);
-            } else {
+    public User getUser(int id) throws IOException{
+        synchronized(users) {
+            if( !users.containsKey(id) )
                 return null;
-            }
+                
+            return users.get(id);
         }
     }
 
     @Override
     public User createUser(User user) throws IOException {
-        synchronized(users){
-            //TODO redo this horrifying piece
-            nextId();
-            return null;
+        synchronized(users) {
+            User newUser = new User(nextId(),
+                                    user.getUsername(),
+                                    user.getCart());
+            users.put(newUser.getId(),newUser);
+            save();
+            return newUser;
         }
     }
 
     @Override
     public User updateUser(User user) throws IOException {
-        synchronized(users){
-            if(users.containsKey(user.getId())){
-                users.put(user.getId(), user);
-                save();
-                return user;
-            } else {
+        synchronized(users) {
+            if( !users.containsKey(user.getId()) )
                 return null;
-            }
+
+            users.put(user.getId(),user);
+            save();
+            return user;
         }
     }
-    
+
     @Override
     public boolean deleteUser(int id) throws IOException {
         synchronized(users) {
-            if (users.containsKey(id)) {
-                users.remove(id);
-                save();
-                return true;
-            }
-            else {
+            if( !users.containsKey(id) )
                 return false;
-            }
+
+            users.remove(id);
+            save();
+            return true;
         }
     }
 }
