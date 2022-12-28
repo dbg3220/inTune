88bdf59e5fbf0397d8975799708623ef97be1a43
Damon Gonzalez <dbg3220@rit.edu>
2022-12-28 13:59:18 -0500

    Added services in the viewmodel to handle business logic

---
+++ estore-api/src/main/java/estoreapi/viewmodel/LessonService.java
@@ -0,0 +1,13 @@
+package estoreapi.viewmodel;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Component class to handle the business logic of the lesson resource
+ * 
+ * @author Damon Gonzalez
+ */
+@Component
+public class LessonService {
+    
+}
---
+++ estore-api/src/main/java/estoreapi/viewmodel/ProductService.java
@@ -0,0 +1,13 @@
+package estoreapi.viewmodel;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Component class to handle the business logic of the product resource
+ * 
+ * @author Damon Gonzalez
+ */
+@Component
+public class ProductService {
+    
+}
